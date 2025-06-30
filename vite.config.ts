508f804a8dfa6fac2ceb5a86945daa07a7f1f361
PROTASOV <137809335+AVPROTASOV@users.noreply.github.com>
2025-06-30 16:22:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import AutoImport from 'unplugin-auto-import/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vue-3-crud/',
